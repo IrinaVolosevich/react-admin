b4fb7c36cd51e16599f14faf56b6b2cb99fb1791
slepovsde <slepovsde@gmail.com>
2021-10-30 20:16:38 +0700

    cinema-list

--- src/pages/Admin/CinemaList/CinemaList.js
+++ src/pages/Admin/CinemaList/CinemaList.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import { Grid } from '@material-ui/core';
+import CinemaCard from './../Components/CinemaCard/CinemaCard';
 
 class CinemaList extends Component {
   render() {
-    return <h1>Hello</h1>;
+    return   <div>
+      <Grid container spacing={3}>
+          <Grid
+            item
+            key={Math.random(10)}
+            lg={4}
+            md={6}
+            xs={12}>
+            <CinemaCard/>
+          </Grid>
+      </Grid>
+  </div>;
   }
 }
 
