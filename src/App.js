ed10fa04188ced205eb12f06aa65cf89b4352fe8
Irina Volosevich <i.volosevich@artox.com>
2021-10-30 15:58:58 +0300

    fix(menu): Add menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-// @ts-nocheck
 import React, { Component } from 'react';
-import { ThemeProvider } from '@material-ui/core/styles';
 
-//Redux
 import { Provider } from 'react-redux';
 
 import store from './store';
 import Routes from './Routes';
 
-import { CssBaseline } from '@material-ui/core';
-
 class App extends Component {
   render() {
     return (
         <Provider store={store}>
-          <ThemeProvider>
-            <CssBaseline />
             <Routes />
-          </ThemeProvider>
         </Provider>
     );
   }
