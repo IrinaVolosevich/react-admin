5d32ecbfa244e221d4231126526c662ddcc8ee8b
slepovsde <slepovsde@gmail.com>
2021-10-31 12:17:03 +0700

    addCinema

--- src/Routes.js
+++ src/Routes.js
@@ -6,6 +6,7 @@ import AdminLayout from './layouts/AdminLayout';
 
 //Admin
 const CinemaList = lazy(() => import('./pages/Admin/CinemaList'));
+const AddCinema = lazy(() => import('./pages/Admin/AddCinema'));
 
 const Routes = () => {
   return (
@@ -17,6 +18,12 @@ const Routes = () => {
                 path="/admin/cinemas"
                 layout={AdminLayout}
                 component={CinemaList}
+            />
+            <WithLayoutRoute
+                exact
+                path="/admin/cinemas/add"
+                layout={AdminLayout}
+                component={AddCinema}
             />
             <Route path="*" component={() => '404 NOT FOUND'} />
           </Switch>
